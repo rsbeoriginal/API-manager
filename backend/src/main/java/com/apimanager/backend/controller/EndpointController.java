d86355db2f0ab7769beb9a69f2252110b92216d2
Himanshu Gupta <you@example.com>
2019-03-08 22:08:27 +0530

    added RequestUtil class

--- backend/src/main/java/com/apimanager/backend/controller/EndpointController.java
+++ backend/src/main/java/com/apimanager/backend/controller/EndpointController.java
@@ -6,30 +6,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apimanager.backend.dto.EndpointDTO;
+import com.apimanager.backend.dto.RequestDTO;
+import com.apimanager.backend.dto.ResponseDTO;
+
 @RestController
 @RequestMapping(EndpointController.BASE_PATH)
 public class EndpointController {
 
   public static final String BASE_PATH = "/endpoint";
 
-//  @PostMapping("/")
-//  public ResponseDTO<AnswerDTO> addEndpoint(@RequestBody AnswerDTO answerDTO) {
-//    Answer answer = new Answer();
-//    BeanUtils.copyProperties(answerDTO, answer);
-//    Question question = new Question();
-//    BeanUtils.copyProperties(answerDTO.getQuestionDTO(),question);
-//    answer.setQuestion(question);
-//    ResponseDTO<AnswerDTO> responseDTO;
-//    try {
-//      responseDTO = answerService.answerQuestion(answer);
-//    } catch (Exception e) {
-//      responseDTO = new ResponseDTO<>();
-//      responseDTO.setStatus("failure");
-//      responseDTO.setErrorMessage(e.getMessage());
-//      responseDTO.setResponse(null);
-//    }
-//    return responseDTO;
-//  }
+  @PostMapping("/")
+  public ResponseDTO<EndpointDTO> addEndpoint(@RequestBody RequestDTO<EndpointDTO> requestDTO) {
+    Answer answer = new Answer();
+    BeanUtils.copyProperties(answerDTO, answer);
+    Question question = new Question();
+    BeanUtils.copyProperties(answerDTO.getQuestionDTO(),question);
+    answer.setQuestion(question);
+    ResponseDTO<AnswerDTO> responseDTO;
+    try {
+      responseDTO = answerService.answerQuestion(answer);
+    } catch (Exception e) {
+      responseDTO = new ResponseDTO<>();
+      responseDTO.setStatus("failure");
+      responseDTO.setErrorMessage(e.getMessage());
+      responseDTO.setResponse(null);
+    }
+    return responseDTO;
+  }
 
 
 
