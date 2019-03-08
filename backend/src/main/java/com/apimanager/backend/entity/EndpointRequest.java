8d9f82535a545e1ef8d0fcb91d36a1746812ad61
Himanshu Gupta <you@example.com>
2019-03-08 20:01:37 +0530

    EndpointRequest entity added

--- backend/src/main/java/com/apimanager/backend/entity/EndpointRequest.java
+++ backend/src/main/java/com/apimanager/backend/entity/EndpointRequest.java
@@ -1,9 +1,21 @@
 package com.apimanager.backend.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.json.JSONObject;
 
 @Entity
 public class EndpointRequest {
+
+  @ManyToOne
+  @JoinColumn(name = "endpoint_id")
+  private Endpoint endpoint;
+
+  private String type;
+  private JSONObject content;
+
 
 
 }
