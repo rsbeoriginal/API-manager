f1097cd2ecdf2c4325a3588461b1dbdd8e1d0691
Jay Joshi <jay.joshi@gdn-commerce.com>
2019-03-08 22:20:18 +0530

    add entity for Endpoint response

--- backend/src/main/java/com/apimanager/backend/entity/UserWatchlist.java
+++ backend/src/main/java/com/apimanager/backend/entity/UserWatchlist.java
@@ -1,6 +1,12 @@
 package com.apimanager.backend.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -8,10 +14,50 @@ import javax.persistence.Table;
  * Created on 08 March 2019
  */
 @Entity
-@Table(name = UserFavorite.USER_FAVORITE_TABLE)
-public class UserFavorite {
-  public static final String USER_FAVORITE_TABLE = "user_favorite";
-  private String subscriberId;
-  private Object endPoint;
-  private Object subscriber;
+@Table(name = UserWatchlist.USER_FAVORITE_TABLE)
+public class UserWatchlist {
+  public static final String USER_FAVORITE_TABLE = "user_watchlist";
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name="uuid",strategy = "uuid2")
+  private String watchlistId;
+  @ManyToOne
+  @JoinColumn(name = "endpoint_fragment")
+  private EndPointResponse endPointFragment;
+  @ManyToOne
+  @JoinColumn(name = "subscriber")
+  private UserEnitity subscriber;
+
+  public String getWatchlistId() {
+    return watchlistId;
+  }
+
+  public void setWatchlistId(String watchlistId) {
+    this.watchlistId = watchlistId;
+  }
+
+  public EndPointResponse getEndPointFragment() {
+    return endPointFragment;
+  }
+
+  public void setEndPointFragment(EndPointResponse endPointFragment) {
+    this.endPointFragment = endPointFragment;
+  }
+
+  public UserEnitity getSubscriber() {
+    return subscriber;
+  }
+
+  public void setSubscriber(UserEnitity subscriber) {
+    this.subscriber = subscriber;
+  }
+
+  @Override
+  public String toString() {
+    return "UserWatchlist{" +
+            "watchlistId='" + watchlistId + '\'' +
+            ", endPointFragment=" + endPointFragment +
+            ", subscriber=" + subscriber +
+            '}';
+  }
 }
