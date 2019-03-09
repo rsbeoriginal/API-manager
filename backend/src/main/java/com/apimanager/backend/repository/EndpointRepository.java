9168828127b669a4ba6fed5a40d0bf757f162c55
Himanshu Gupta <you@example.com>
2019-03-09 13:03:16 +0530

    added method in endpoint controller

--- backend/src/main/java/com/apimanager/backend/repository/EndpointRepository.java
+++ backend/src/main/java/com/apimanager/backend/repository/EndpointRepository.java
@@ -1,5 +1,8 @@
 package com.apimanager.backend.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import com.apimanager.backend.entity.Endpoint;
 @Repository
 public interface EndpointRepository extends CrudRepository<Endpoint, String> {
 
+  @Query(value = "select * from endpoint where project_id=?1",nativeQuery = true)
+  List<Endpoint> getEndpointByProjectId(String projectId);
 }
