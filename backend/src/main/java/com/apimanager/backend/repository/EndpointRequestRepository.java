27c1d092293b6ccbeef8f757b257d429dde355c6
Himanshu Gupta <you@example.com>
2019-03-09 11:49:38 +0530

    modified get method

--- backend/src/main/java/com/apimanager/backend/repository/EndpointRequestRepository.java
+++ backend/src/main/java/com/apimanager/backend/repository/EndpointRequestRepository.java
@@ -1,5 +1,7 @@
 package com.apimanager.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,8 @@ import com.apimanager.backend.entity.EndpointRequest;
 @Repository
 public interface EndpointRequestRepository extends CrudRepository<EndpointRequest,String> {
 
-  EndpointRequest findEndpointRequestByEndpointId(String endpointId);
+  @Query(value = "select * from endpoint_request where endpoint_id=?1",nativeQuery = true)
+  List<EndpointRequest> selectEndpointRequestByEndpointId(String endpointId);
 
   @Query(value = "select max(version) from endpoint_request where endpoint_id=?1",nativeQuery = true)
   int getMaxVersion(String endpointId);
